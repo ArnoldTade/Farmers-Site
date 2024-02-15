62c3acf27c1e31511556d42a628c78d10b6cfa9a
Arnold Saldivar <saldivararnold80@gmail.com>
2024-02-15 21:25:45 +0800

    modified a lot

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -49,8 +49,7 @@ class Order(models.Model):
         shipping = False
         orderitems = self.orderitem_set.all()
         for i in orderitems:
-            if i.product.digital == False:
-                shipping = True
+            shipping = True
         return shipping
 
     @property
@@ -87,6 +86,8 @@ class ShippingAddress(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     address = models.CharField(max_length=200, null=True)
     city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
     date_Added = models.CharField(max_length=200, null=True)
 
     def __str__(self):
